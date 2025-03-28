68fb7393c94a0ed7b0bdba266080fc7742f9dd2f
Eve Aviv Keinan <evavkein@gmail.com>
2025-03-28 21:38:49 +0200

    create colors.hpp

--- module00/ex01/colors.hpp
+++ module00/ex01/colors.hpp
@@ -6,20 +6,34 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 16:31:29 by ekeinan           #+#    #+#             */
-/*   Updated: 2025/03/26 17:17:06 by ekeinan          ###   ########.fr       */
+/*   Updated: 2025/03/28 18:03:46 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef COLOR
-# define COLOR
+#ifndef COLOR_HPP
+# define COLOR_HPP
 
-# define CLR_RESET	"\e[0;37m"
-# define CLR_DIM 	"\e[2;37m"
-// # define CLR_THINK	"\e[1;35m"
-// # define CLR_FORK 	"\e[2;32m"
-// # define CLR_EAT 	"\e[1;32m"
-// # define CLR_SLEEP	"\e[1;36m"
-// # define CLR_DIED 	"\e[1;31m"
-// # define CLR_TIME 	"\e[1;30m"
-
+# if COLOR
+#  define CLR_RESET			"\e[0m"
+#  define CLR_BACKGROUND	"\e[7m"
+#  define CLR_YLLWBGBOLD	"\e[7;1;33m"
+#  define CLR_DIM 			"\e[2;37m"
+#  define CLR_DIMBOLD		"\e[2;1;37m"
+#  define CLR_RED			"\e[0;31m"
+#  define CLR_REDBOLD		"\e[1;31m"
+#  define CLR_GREEN			"\e[0;32m"
+#  define CLR_GREENBOLD		"\e[1;32m"
+# else
+#   define COLOR 0
+#   define CLR_RESET		""
+#   define CLR_BACKGROUND	""
+#   define CLR_YLLWBGBOLD	""
+#   define CLR_DIM 			""
+#   define CLR_DIMBOLD		""
+#   define CLR_RED			""
+#   define CLR_REDBOLD		""
+#   define CLR_GREEN		""
+#   define CLR_GREENBOLD	""
 # endif
+
+#endif
