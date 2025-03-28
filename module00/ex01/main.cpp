ecc251c108942a63bf713a3a972d6b3ed49ee6b0
Eve Aviv Keinan <evavkein@gmail.com>
2025-03-28 21:40:58 +0200

    various main.cpp updates

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 17:36:30 by ekeinan           #+#    #+#             */
-/*   Updated: 2025/03/25 15:07:21 by ekeinan          ###   ########.fr       */
+/*   Updated: 2025/03/28 12:27:11 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,36 @@
 int main(int argc, char **argv)
 {
 	if (argc > 1)
-		std::cout << "* LOUD AND UNBEARABLE PHONEBOOK NOISE *"
-		<< "\n(No args, \"" << argv[0] << "\" is good enough ^^)" << std::endl;
+		std::cout << CLR_DIMBOLD "* LOUD AND UNBEARABLE PHONEBOOK NOISE *\n" CLR_RESET
+		CLR_DIM "(No args, \"" << argv[0] << "\" is good enough ^^)" CLR_RESET << std::endl;
 		
 	PhoneBook phonebook = PhoneBook();
 	std::string input;
 	while (std::getline(std::cin, input))
 	{
-		if (!input.compare("ADD"))
+		if (input == "ADD")
 		{
-			phonebook.add_contact();
+			if (!phonebook.add_contact())
+			{
+			 	std::cout << "Exiting phonebook!" << std::endl;
+				return EXIT_SUCCESS;
+			}
 		}
-		else if (!input.compare("SEARCH"))
+		else if (input == "SEARCH")
 		{
 			phonebook.search_contacts();
 		}
-		else if (!input.compare("EXIT"))
+		else if (input == "EXIT")
 		{
 			std::cout << "Exiting phonebook!" << std::endl;
-			return (EXIT_SUCCESS);
+			return EXIT_SUCCESS;
 		}
 		else
-			std::cout
-				<< "This phonebook can only \"ADD\", \"SEARCH\", or \"EXIT\"!"
+			std::cerr <<
+				CLR_REDBOLD "This phonebook can only "
+				CLR_GREENBOLD "ADD" CLR_RESET CLR_REDBOLD ", " CLR_RESET
+				CLR_GREENBOLD "SEARCH" CLR_RESET CLR_REDBOLD ", or " CLR_RESET
+				CLR_GREENBOLD "EXIT" CLR_RESET CLR_REDBOLD "!" CLR_RESET
 			<< std::endl;	
 	}
 	return EXIT_FAILURE;
