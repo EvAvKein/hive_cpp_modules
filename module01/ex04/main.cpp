7adc2e1224f8c92f88fdc714db6722a8f4487708
Eve Aviv Keinan <evavkein@gmail.com>
2025-04-03 19:24:07 +0300

    updates to module01/ex04/main.cpp

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,58 +6,82 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 22:07:11 by ekeinan           #+#    #+#             */
-/*   Updated: 2025/04/02 08:51:42 by ekeinan          ###   ########.fr       */
+/*   Updated: 2025/04/03 08:53:18by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char** argv)
+typedef	struct		s_data
+{
+	std::ifstream	infile;
+	std::string		s1;
+	std::string		s2;
+	size_t			s1_len;
+	std::ofstream	outfile;
+}					t_data;
+
+static inline void prep_and_validation(int &argc, char **argv, t_data &data)
 {
 	if (argc != 4)
-	{
-		std::cout <<
-			"SIFL needs exactly 3 arguments: "
-			"file, string to replace, and string that'll be the replacement"
-		<< std::endl;
-		return EXIT_FAILURE;
+		throw std::string("SIFL needs exactly 3 arguments: file, string to replace, and string that'll be the replacement");
+
+	data.s1 = argv[2];
+	data.s1_len = data.s1.length();
+	if (!data.s1_len)
+		throw std::string("SIFL's string to replace (second arg) must not be empty");
+	
+	data.s2 = argv[3];
+	
+	try {
+		data.infile = std::ifstream(argv[1]);
+	} catch (std::string error) {
+		throw std::move(error);
 	}
 
-	size_t	s1_len = std::string(argv[2]).length();
-	if (!s1_len)
-	
-	size_t	s2_len = std::string(argv[3]).length();
-	
-	std::ifstream infile;
 	try {
-		infile = std::ifstream(argv[1]);
-	} catch (std::exception error) {
-		std::cout << "SIFL: " << error.what() << std::endl;
-		return EXIT_FAILURE;
+		data.outfile = std::ofstream(std::string(argv[1]).append(".replace"));
+	} catch (std::string error) {
+		data.infile.close();
+		throw std::move(error);
 	}
+}
+int main(int argc, char** argv)
+{
+	t_data	data;
 
-	std::ofstream outfile;
-	try {
-		outfile = std::ofstream(std::string(argv[1]).append(".replace"));
-	} catch (std::exception error) {
-		infile.close();
-		std::cout << "SIFL: " << error.what() << std::endl;
+	try
+	{
+		prep_and_validation(argc, argv, data);
+	}
+	catch (std::string& error)
+	{
+		std::cerr << error << std::endl;
 		return EXIT_FAILURE;
 	}
 	
-	std::string	input;
-	std::string match;
-	while (getline(infile, input))
+	std::string		input;
+	size_t			match_i;
+	while (getline(data.infile, input, '\0'))
 	{
-		match = input.find();
-		if (match == std::string::npos)
+		match_i = input.find(data.s1, 0);
+
+		if (match_i == std::string::npos)
+			data.outfile << input;
+		else
 		{
-			in
+			while (match_i != std::string::npos)
+			{
+				data.outfile << input.substr(0, match_i).append(data.s2);
+				input.erase(0, match_i + data.s1_len);
+				match_i = input.find(data.s1, 0);
+			}
 		}
 	}
-
-	infile.close();
+	
+	data.infile.close();
+	data.outfile.close();
 
 	return EXIT_SUCCESS;
 }
