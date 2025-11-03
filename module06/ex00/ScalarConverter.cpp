1008107137bdcae877a64b12fd624743c3c8f61b
Eve Keinan <evavkein@gmail.com>
2025-11-03 15:14:30 +0200

    module06/ex00 progress

--- module06/ex00/ScalarConverter.cpp
+++ module06/ex00/ScalarConverter.cpp
@@ -12,31 +12,33 @@
 
 #include "ScalarConverter.hpp"
 
-void	ScalarConverter::convert(std::string_view str)
+void ScalarConverter::convert(std::string_view str)
 {
-	literalType type = parseType(str);
+	std::string convertStr(str);
+
+	literalType type = parseType(convertStr);
 
 	switch (type)
 	{
-		case INVALID: {
-
-		}
-		case CHAR: {
-
-		}
-		case INT: {
-
-		}
-		case FLT: {
-
-		}
-		case DBL: {
-
-		}
-		default: {
-			std::cout << "Impossible condition" << std::endl;
-		}
-
+	case INVALID:
+	{
+	}
+	case CHAR:
+	{
+	}
+	case INT:
+	{
+	}
+	case FLT:
+	{
+	}
+	case DBL:
+	{
+	}
+	default:
+	{
+		std::cout << "Impossible condition" << std::endl;
+	}
 	}
 
 	printStrToChar(str);
@@ -45,56 +47,83 @@ void	ScalarConverter::convert(std::string_view str)
 	printStrToDouble(str);
 }
 
-ScalarConverter::literalType	ScalarConverter::parseType(std::string_view str)
+ScalarConverter::literalType ScalarConverter::parseType(std::string &str)
 {
 	size_t strlen = str.length();
-
 
 	if (strlen == 0)
 		return INVALID;
 
-	if ( (strlen == 1 && !std::isdigit(str[0]) )
-		|| (strlen == 3 && str[0] == '\'' && str[2] == '\'') )
-		return CHAR;
+	if (str == "+inf" || str == "-inf" || str == "nan")
+		return DBL;
+
+	if (str == "+inff" || str == "-inff" || str == "nanf")
+		return FLT;
 
 	if (str.find_first_not_of("0123456789.f-+") != str.npos)
 		return INVALID;
 
-	auto c = str.find_first_of('.');
-	if (true) // TODO conditions for decimal dup/misplacement :P
-		;
+	if ((strlen == 1 && !std::isdigit(str[0])) || (strlen == 3 && str[0] == '\'' && str[2] == '\''))
+		return CHAR;
 
-	if (str.find_first_of(".f") == str.npos)
-		  return INT;
+	size_t signIndex = str.find_last_of('-+');
+	if (signIndex != str.npos && signIndex != 0)
+		return INVALID;
 
-	else if ((
-				str.find_last_of('f') != str.npos
-				&& (str[strlen - 1] != 'f'
-					|| str.find_first_of('f') != str.find_last_of('f'))
-			 ))
-		;
-	
+	size_t fCharIndex = str.find_first_of('f');
+	if (fCharIndex != str.npos && fCharIndex != strlen - 1)
+		return INVALID;
+
+	size_t decimalIndex = str.find_first_of('.');
+	if (decimalIndex != str.npos && decimalIndex != str.find_last_of('.'))
+		return INVALID;
+
+	if (decimalIndex == str.npos && fCharIndex != str.npos)
+		return INVALID;
+
+	bool hasWholeDigit = false;
+	bool hasFractionalDigit = false;
+	for (size_t i = 0; i < strlen; i++)
+	{
+		if (std::isdigit(str[i]))
+		{
+			if (i < decimalIndex)
+				hasWholeDigit = true;
+			else
+				hasFractionalDigit = true;
+		}
+	}
+
+	if (!hasWholeDigit && !hasFractionalDigit)
+		return INVALID;
+
+	if (!decimalIndex && fCharIndex == str.npos)
+		return INT;
+
+	if (fCharIndex != str.npos)
+		return FLT;
+
+	return DBL;
 }
 
-void	ScalarConverter::printStrToChar(std::string_view str)
+void ScalarConverter::printStrToChar(std::string_view str)
 {
-	char c = *static_cast<char*>((char*)str.data());
+	char c = *static_cast<char *>((char *)str.data());
 
 	std::cout << "char: " << c << std::endl;
 }
 
-void	ScalarConverter::printStrToInt(std::string_view str)
+void ScalarConverter::printStrToInt(std::string_view str)
 {
-	(void) str;
+	(void)str;
 }
 
-void	ScalarConverter::printStrToFloat(std::string_view str)
+void ScalarConverter::printStrToFloat(std::string_view str)
 {
-	(void) str;
+	(void)str;
 }
 
-void	ScalarConverter::printStrToDouble(std::string_view str)
+void ScalarConverter::printStrToDouble(std::string_view str)
 {
-	(void) str;
+	(void)str;
 }
-
--- module06/ex00/ScalarConverter.hpp
+++ module06/ex00/ScalarConverter.hpp
@@ -12,38 +12,37 @@
 
 #pragma once
 #ifndef SCALAR_CONVERTER_HPP
-# define SCALAR_CONVERTER_HPP
+#define SCALAR_CONVERTER_HPP
 
-# include <string_view>
-# include <iostream>
+#include <string_view>
+#include <iostream>
 
 class ScalarConverter
 {
-	public:
-		ScalarConverter(void) = delete;
-		ScalarConverter(ScalarConverter const& copied) = delete;
-		ScalarConverter &operator=(ScalarConverter const& assigned) = delete;
-		~ScalarConverter(void) = delete;
+public:
+	ScalarConverter(void) = delete;
+	ScalarConverter(ScalarConverter const &copied) = delete;
+	ScalarConverter &operator=(ScalarConverter const &assigned) = delete;
+	~ScalarConverter(void) = delete;
 
-		static void	convert(std::string_view str);
+	static void convert(std::string_view str);
 
-	private:
-		enum literalType {
-			INVALID,
-			CHAR,
-			INT,
-			FLT,
-			DBL,
-		};
+private:
+	enum literalType
+	{
+		INVALID,
+		CHAR,
+		INT,
+		FLT,
+		DBL,
+	};
 
-		static literalType	parseType(std::string_view str);
+	static literalType parseType(std::string &str);
 
-		
-		static void printStrToChar(std::string_view str);
-		static void printStrToInt(std::string_view str);
-		static void printStrToFloat(std::string_view str);
-		static void printStrToDouble(std::string_view str);
-
+	static void printStrToChar(std::string_view str);
+	static void printStrToInt(std::string_view str);
+	static void printStrToFloat(std::string_view str);
+	static void printStrToDouble(std::string_view str);
 };
 
 #endif
