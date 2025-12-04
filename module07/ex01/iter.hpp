61fd686a0dd39f8a846a1324ee7701942b46b57d
Eve Aviv Keinan <evavkein@gmail.com>
2025-12-04 18:22:18 +0200

    mod07/ex01 header inclusion refactor

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -14,7 +14,7 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-#include <iostream>
+#include <cstddef>
 
 template <class T>
 void	iter(T* array, size_t length, void (*func)(T&))
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "iter.hpp"
+#include <iostream>
 
 void print(std::string_view& string) {
 	std::cout << string << std::endl;
